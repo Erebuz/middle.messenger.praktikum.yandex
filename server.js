e32a009bbb816ea077c2e223cfbf4c841add989c
Dmitrij Pokladov <volhv8@yandex.ru>
2023-02-20 23:44:32 +0300

    Неудачно, возврат

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`)
 })
 
 app.use('/*', (req, res) => {
-  res.sendFile('dist/index.html')
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
- 
